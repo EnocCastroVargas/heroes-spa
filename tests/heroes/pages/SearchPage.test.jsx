41dbd87601acb15990536dcfa519701dcfd396a1
Enoc Castro Vargas <enocco70@gmail.com>
2022-07-14 11:58:00 -0600

    pruebas en el SearchPage

---
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter, useNavigate } from 'react-router-dom'; 
+import { SearchPage } from '../../../src/heroes/pages/SearchPage';
+
+describe('Pruebas en <SearchPage />', () => {
+    
+    test('debe de mostrarse correctamente con valores por defecto', () => {
+        
+        const { container } = render( 
+            <MemoryRouter>
+                <SearchPage /> 
+            </MemoryRouter>
+        );
+
+        expect( container ).toMatchSnapshot();
+
+    });
+
+});
