7bb4cc457e91a718c5ba9cf40b43200753753efd
Enoc Castro Vargas <enocco70@gmail.com>
2022-07-14 10:41:00 -0600

    pruebas en PrivateRoute

---
+++ tests/router/PrivateRoute.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../src/auth';
+import { PrivateRoute } from '../../src/router/PrivateRoute';
+
+
+describe('Pruebas en <PrivateRoute />', () => {
+
+    test('debe de mostrar el children, si está autenticado', () => {
+
+        Storage.prototype.setItem = jest.fn();
+        
+        const contextValue = {
+            logged: true,
+            user: {
+                id: 'ABC',
+                name: 'Enoc Castro'
+            }
+        };
+
+        render( 
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter initialEntries={['/marvel']}>
+                    <PrivateRoute>
+                        <h1>Ruta privada</h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+            </AuthContext.Provider>
+         );
+
+        expect( screen.getByText('Ruta privada') ).toBeTruthy();
+        expect( localStorage.setItem ).toHaveBeenCalled();
+        expect( localStorage.setItem ).toHaveBeenCalledWith("lastPath", "/marvel");
+
+    });
+
+});
